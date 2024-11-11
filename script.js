a91601f8163fc794ff87afe0002902e86ab6a893
TomefaStudio <sergesvenceslas.com>
2024-11-11 11:50:59 -0800

    tomefa studio 0

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-//Désactiver le scrol de la page dès le début du chargement !
 document.body.style.overflow = "hidden";
 //Créer une instance du loader et de son conteneur !
 const loaderContainer = document.querySelector(".loaderContainer");
-//Faire disparaitre le loader une fois la page complètement chargée !
-loaderContainer.classList.add("fonduOut");
-setTimeout((e) => {
-    loaderContainer.classList.add("hidden");
-}, 400);
+//Faire disparaitre le loader et son conteneur !
+window.addEventListener("load",(e)=>{
+    loaderContainer.classList.add("fonduOut");
+    setTimeout((e) => {
+        loaderContainer.classList.add("hidden");
+    },400);
+});
